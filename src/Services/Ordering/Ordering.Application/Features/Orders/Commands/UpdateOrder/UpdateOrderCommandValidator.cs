4b0569f6d6169fcf5a770f568f8cfd28ac8473bb
Hải Nguyễn <hoanghai03021999@gmail.com>
2023-03-08 16:22:37 +0700

    cap nhat code order ngay 7/3

---
+++ src/Services/Ordering/Ordering.Application/Features/Orders/Commands/UpdateOrder/UpdateOrderCommandValidator.cs
@@ -0,0 +1,27 @@
+﻿using FluentValidation;
+using Ordering.Application.Features.Orders.Commands.CheckoutOrder;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ordering.Application.Features.Orders.Commands.UpdateOrder
+{
+    public class UpdateOrderCommandValidator : AbstractValidator<UpdateOrderCommand>
+    {
+        public UpdateOrderCommandValidator()
+        {
+            RuleFor(p => p.UserName).NotEmpty().WithMessage("Phai nhap dcmm")
+                .NotNull()
+                .MaximumLength(50).WithMessage("dai qua dcmm");
+
+            RuleFor(p => p.EmailAddress)
+               .NotEmpty().WithMessage("{EmailAddress} is required.");
+
+            RuleFor(p => p.TotalPrice)
+                .NotEmpty().WithMessage("{TotalPrice} is required.")
+                .GreaterThan(0).WithMessage("{TotalPrice} should be greater than zero.");
+        }
+    }
+}
